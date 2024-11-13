23e7895311d984bb36a99c9ec4c1b72b42efef0c
mateocm <mateo.cespedes@ie-consulting.net>
2024-11-13 17:19:04 -0500

    se agregan rutas y controladores para manejar la logica al final de las rutas usando laravel 11, ya que lo que aparece en el video es para laravel 7

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index () {
+        return 'This is the list of products FROM CONTROLLER';
+    }
+
+    public function create() {
+        return 'This is the form to create a product FROM CONTROLLER';
+    }
+
+    public function store() {
+        //
+    }
+
+    public function show ($product) {
+        return 'Showing product with id {$product}';
+    }
+    public function edit ($product) {
+        return 'Showing product with the form to edit the product with id {$product}';
+    }
+    public function update ($product) {
+        //
+    }
+    public function destroy($product) {
+        //
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// })->name('main');
+
+Route::get('products', [ProductController::class, 'index'])->name('products.index'); // el mensaje lo estoy retornando en el controlador, llamo la funcion del controlador con el ProductController@index--> funciona solo en laravel 7 hacia abajo
+ //para laravel 7 hacia arriba se usa --> Route::get('/', [NameController::class, 'helo']); se manda en un arreglo la primera posicion es el espacio de nombre del controlador y la segunda es el nombre de la acción dentro del controlador (o nombre de la funcion en este caso) el '/' se refiere a la carpeta raiz, en este caso public
+
+//por ejemplo antes estaban así:
+// Route::get('products/create', function () {
+//     return 'This is the form to create a product';
+// })->name('products.create');
+
+Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
+
+Route::post('products',[ProductController::class, 'store'])->name('products.store');
+
+// Route::get('products/{product}', function ($product) {// puede ser un string, un id, etc
+//     return "Showing product with id {$product}"; //se usa un parametro (parametro en la ruta) el parametro es obligatorio
+// })->name('products.show'); //como lo pido php me lo permite mandar
+
+Route::get('products/{product}',[ProductController::class, 'show'])->name('products.show');
+
+Route::get('products/{product}/edit',[ProductController::class, 'edit'])->name('products.edit');
+
+Route::match(['put', 'patch'], 'products/{product}', [ProductController::class, 'update'])->name('products.update');
+
+Route::delete('products/{product}/edit', [ProductController::class, 'destroy'])->name('products.destroy');
